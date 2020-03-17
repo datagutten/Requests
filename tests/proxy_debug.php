01d30408325452167b7ff2db44fd7a41e56e8761
datagutten <datagutten@datagutten.net>
2020-03-17 13:20:07 +0100

    Add transport argument

--- tests/proxy_debug.php
+++ tests/proxy_debug.php
@@ -3,7 +3,8 @@ require '../vendor/autoload.php';
 
 foreach (['Requests_Transport_fsockopen', 'Requests_Transport_cURL'] as $transport) {
 	$options = array(
-		'proxy' => 'localhost:9002'
+		'proxy' => 'localhost:9002',
+		'transport' => $transport
 	);
 
 	$response = Requests::get('http://requests-php-tests.herokuapp.com/get', array(), $options);
