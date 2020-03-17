9b5219ccdbd6cc85438eddea822c95c969dda102
datagutten <datagutten@datagutten.net>
2020-03-17 13:17:23 +0100

    Try both cURL and fsockopen

--- tests/proxy_debug.php
+++ tests/proxy_debug.php
@@ -1,9 +1,12 @@
 <?php
 require '../vendor/autoload.php';
-$options = array(
-	'proxy' => 'localhost:9002'
-);
 
-$response = Requests::get('http://requests-php-tests.herokuapp.com/get', array(), $options);
-var_dump($response->headers['x-requests-proxied']);
-print_r($response);
+foreach (['Requests_Transport_fsockopen', 'Requests_Transport_cURL'] as $transport) {
+	$options = array(
+		'proxy' => 'localhost:9002'
+	);
+
+	$response = Requests::get('http://requests-php-tests.herokuapp.com/get', array(), $options);
+	var_dump($response->headers['x-requests-proxied']);
+	print_r($response);
+}
