c89cb508a6ae2a07ad6db1a496306446e7cfb7be
datagutten <datagutten@datagutten.net>
2020-03-17 13:07:58 +0100

    httpbin

--- tests/proxy_debug.php
+++ tests/proxy_debug.php
@@ -3,6 +3,7 @@ require '../vendor/autoload.php';
 $options = array(
 	'proxy' => 'localhost:9002'
 );
-$response = Requests::get(httpbin('/get'), array(), $options);
+
+$response = Requests::get('http://requests-php-tests.herokuapp.com/get', array(), $options);
 var_dump($response->headers['x-requests-proxied']);
 print_r($response);
